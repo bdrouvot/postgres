e2690d8666a454a500ba1a34373920bad6eb2496
Bertrand Drouvot <bertranddrouvot.pg@gmail.com>
2025-03-31 07:02:34 +0000

    Fix heap-use-after-free in pgstat_fetch_stat_backend_by_pid()
    With stats_fetch_consistency set to snapshot the beentry is reset during
    the pgstat_fetch_stat_backend() call. So moving this call at the end of
    pgstat_fetch_stat_backend_by_pid().
    
    Reported-by: Alexander Lakhin <exclusion@gmail.com>

--- src/backend/utils/activity/pgstat_backend.c
+++ src/backend/utils/activity/pgstat_backend.c
@@ -133,16 +133,16 @@ pgstat_fetch_stat_backend_by_pid(int pid, BackendType *bktype)
 	if (!pgstat_tracks_backend_bktype(beentry->st_backendType))
 		return NULL;
 
-	backend_stats = pgstat_fetch_stat_backend(procNumber);
-	if (!backend_stats)
-		return NULL;
-
 	/* if PID does not match, leave */
 	if (beentry->st_procpid != pid)
 		return NULL;
 
 	if (bktype)
 		*bktype = beentry->st_backendType;
+
+	backend_stats = pgstat_fetch_stat_backend(procNumber);
+	if (!backend_stats)
+		return NULL;
 
 	return backend_stats;
 }
